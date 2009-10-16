2e45c7dc8b9bc622a0df20fe198ae0c099ce927d
looloobs <laurenrothlisberger@Macintosh.local>
2009-10-15 22:53:32 -0500

    remove

--- db/migrate/20090720190931_add_neighborhood_to_onposts.rb
+++
@@ -1,9 +0,0 @@
-class AddNeighborhoodToOnposts < ActiveRecord::Migration
-  def self.up 
-    remove_column :onposts, :neighborhood_id, :string
-  end
-
-  def self.down
-    remove_column :table_name, :column_name
-  end
-end
